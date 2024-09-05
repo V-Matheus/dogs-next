d574e8a351c7479a427cfab5eb85561598e8a69f
Victor Matheus <victormatheus507@gmail.com>
2024-09-05 15:15:25 -0300

    login estilos

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -6,6 +6,8 @@ import Button from '@/components/forms/button';
 import Input from '@/components/forms/input';
 import ErrorMessage from '../helper/error-message';
 import { useEffect } from 'react';
+import Link from 'next/link';
+import styles from './login-form.module.css';
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -33,13 +35,21 @@ export default function LoginForm() {
   }, [state.ok]);
   return (
     <>
-      <form action={action}>
+      <form action={action} className={styles.form}>
         <Input label="Usuário" name="username" type="text" />
         <Input label="Senha" name="password" type="password" />
 
         <FormButton />
         <ErrorMessage error={state.error} />
       </form>
+      <Link className={styles.perdeu} href="/login/perdeu">
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Aida não possui conta? Cadastre-se no site.</p>
+        <Link className='button' href='/login/criar'>Cadastro</Link>
+      </div>
     </>
   );
 }
